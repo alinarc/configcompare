44ed314a8c3efcabe974c86d5b3ba8bd0d21c24d
alina <alina@alinas-mbp-6>
2021-03-02 17:31:58 -0700

    used cellVnom to compute the required Ah of cells, added module kWh to table

--- pack_compare_ac_v4.m
+++ pack_compare_ac_v4.m
@@ -18,7 +18,8 @@ cellVnom = (cellVmax + cellVmin)/2; % use mean cellV to compute modulekWh?
 nCellSer = round(moduleV./cellVmax);
 moduleVmax = cellVmax * nCellSer;
 moduleVmin = cellVmin * nCellSer;
-reqdAh = modulekWh * 1000 ./ moduleVmax;
+moduleVnom = cellVnom * nCellSer;
+reqdAh = modulekWh * 1000 ./ moduleVnom;
 
 nCellPar = ceil(reqdAh ./ cellAh);
 moduleAh_actual = nCellPar .* cellAh;
@@ -37,6 +38,7 @@ packkWh_actual = modulekWh_actual .* nModSer .* nModPar;
 
 moduleAh_t = reshape(moduleAh_actual, size(modulekWh,1)*size(moduleV,1),1);
 nBalPack_t = reshape(nBalPack, size(moduleAh_t));
+modulekWh_actual_t = reshape(modulekWh_actual,size(moduleAh_t));
 packkWh_actual_t = reshape(packkWh_actual, size(moduleAh_t));
 
 packConfig = cell(size(moduleAh_t));
@@ -45,14 +47,14 @@ for i = 1:size(moduleAh_t)
     packConfig{i} = sprintf('%ds%dp', nModSer, nModPar(i));
 end
 packConfig = string(packConfig);
-varNames = {'Module Ah', 'Pack config', '# bal circuits/pack', 'Pack capacity (kWh)'};
+varNames = {'Module Ah', 'Module kWh', 'Pack config', '# bal circuits/pack', 'Pack capacity (kWh)'};
 rowNames = cell(size(modulekWh,1), size(moduleV,1));
 for i = 1:size(modulekWh)
     rowNames{i} = sprintf('%g kWh module', modulekWh(i));
 end
 title = sprintf('Pack comparisons for %g-%g kWh modules. \n Vmod = %g-%g V, Vpack = %g-%g V', ...
     min(modulekWh), max(modulekWh), moduleVmin, moduleVmax, min(packV), max(packV));
-C = {moduleAh_t, packConfig, nBalPack_t, packkWh_actual_t};
+C = {moduleAh_t, modulekWh_actual_t, packConfig, nBalPack_t, packkWh_actual_t};
 T = table(C{:});
 T.Properties.RowNames = rowNames;
 T.Properties.VariableNames = varNames;
